ee192ed4d99318ff6aa519e4a812316d82b49373
Jajack <bernardobpl97@gmail.com>
2020-05-05 23:04:40 -0300

    HTML tags, CSS variable and web-design //END

--- border-radius-preview/src/App.js
+++ border-radius-preview/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const root = document.documentElement;
+  const [radius, setRadius] = useState(0);
+  
+  function handleRadius(){
+    root.style.setProperty('--bd-rd', radius);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
+        <h1> Border Radius Preview </h1>
       </header>
+
+      <section>
+        <textarea value={`Radius=${radius}px`} readOnly onChange={handleRadius()}/>
+        <br/>
+        <label>Border Radius </label>
+        <input min="0" max="105" value={radius} onChange={e => setRadius(e.target.value)}/>
+      </section>
+
     </div>
   );
 }
